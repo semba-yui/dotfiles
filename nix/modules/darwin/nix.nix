1c8d9a6667a8eb64f171ccdcc3084c68fbd506b4
仙波 琉一朗 / Ryuichiro Semba <65758369+semba-yui@users.noreply.github.com>
2026-07-16 03:41:49 +0900

    feat: Nix store の自動保守を設定

--- nix/modules/darwin/nix.nix
+++ nix/modules/darwin/nix.nix
@@ -1,6 +1,15 @@
 {
-  nix.settings.experimental-features = [
-    "flakes"
-    "nix-command"
-  ];
+  nix = {
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+    };
+
+    optimise.automatic = true;
+
+    settings.experimental-features = [
+      "flakes"
+      "nix-command"
+    ];
+  };
 }
